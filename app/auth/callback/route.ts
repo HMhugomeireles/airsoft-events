1075b30273f932b7f35ee591805f4361032601b2
HMhugomeireles <meireles.hmm@mail.com>
2024-06-28 22:47:04 +0100

    change redirect

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,10 +1,11 @@
 import { createClient } from "@/utils/supabase/server";
+import { defaultUrl } from "@/utils/utils";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
-  const origin = requestUrl.origin;
+  const origin = defaultUrl
 
   const supabase = createClient();
   if (code) {
